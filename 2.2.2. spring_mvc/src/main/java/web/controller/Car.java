2347c661dc44259af1e74afa96f2459c798b6191
Charly Breadberry <aleks_inter@mail.ru>
2021-01-30 15:22:33 +0300

    create cars

---
+++ 2.2.2. spring_mvc/src/main/java/web/controller/Car.java
@@ -0,0 +1,59 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Car {
+    private String model;
+    private int number;
+    private String color;
+
+    public Car(){
+
+    }
+    public Car(String model, int number, String color) {
+        this.model = model;
+        this.number = number;
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", number=" + number +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
---
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
@@ -0,0 +1,38 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class CarController {
+
+    private static final List<Car> cars = Arrays.asList(
+            new Car("BMW", 1, "black"),
+            new Car("Mersedes", 2, "white"),
+            new Car("Reno", 3, "yellow"),
+            new Car("Audi", 4, "red"),
+            new Car("Ford", 5, "green"));
+
+
+    public List<Car> getSomeCars(Integer count) {
+        if (count == null || count >= 5) {
+            return cars;
+        }
+
+        return cars.subList(0, count);
+    }
+
+    @GetMapping("/cars")
+    public String showSomeCars(@RequestParam(value = "count", required = false) Integer count, Model model) {
+
+        model.addAttribute("cars", getSomeCars(count));
+        return "cars";
+    }
+
+}
