f0eafda9408f8fbb48130fdeba7c86c638a4a2d3
Charly Breadberry <aleks_inter@mail.ru>
2021-02-01 11:00:35 +0300

    second commit

--- 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import web.model.Car;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
